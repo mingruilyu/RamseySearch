3acc82c81d943c5e2113f91e46c9c82bffe91774
Yanhong Ju <yanhongju@umail.ucsb.edu>
2015-05-21 15:43:21 -0700

    Update graph.h

--- src/headers/graph.h
+++ src/headers/graph.h
@@ -2,6 +2,7 @@
 #define GRAPH_UTILS_H
 
 void PrintGraph(int *g, int gsize);
+void PrintGraphCopy(int *g, int gsize, int graph_count);
 int ReadGraph(char *fname, int **g, int *gsize);
 void* DegradeGraph(int *old_g, int o_gsize);
 void CopyGraph(int *old_g, int o_gsize, int *new_g, int n_gsize);

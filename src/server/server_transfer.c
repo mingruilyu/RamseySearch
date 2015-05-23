446da41cc17612f6e883ee4db45f3515258214f0
Shenghan Gao <victorgaohk@gmail.com>
2015-05-22 22:04:03 -0700

    par fix

--- src/server/server_transfer.c
+++ src/server/server_transfer.c
@@ -307,7 +307,7 @@ void *server_listen_to_clients_handler() {
 	int connectedSocket;
 	
 	while (1) {
-		connectedSocket = accept(serv_socket, (struct sockaddr*)&client_addr, &length));
+		connectedSocket = accept(serv_socket, (struct sockaddr*)&client_addr, &length);
 		perror("While loop! accept error :");
 		if (connectedSocket != 0) {
 			perror("accept error :");

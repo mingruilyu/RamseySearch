af38ccc8e212cae171e639343942fb6b924a8a60
Mingrui Lyu <mingrui_lyu@bugs.cs.ucsb.edu>
2015-05-22 23:06:15 -0700

    try

--- src/client/client_transfer.c
+++ src/client/client_transfer.c
@@ -36,15 +36,15 @@ void send_file(int connected_socket) {
 	else {
 		int file_block_length = 0;
 		//printf("Entering the read while block!\n");
-		while ((file_block_length = fread(buffer, sizeof(char), BUFFER_SIZE, fp)) > 0) {
+		while ((file_block_length = fread(buffer, sizeof(char), BUFFER_SIZE, fp)) >= 0) {
 			printf("file_block_length = %d\n", file_block_length);
 			printf("buffer:  %s \n", buffer);
 			if (send(connected_socket, buffer, file_block_length, 0) < 0) {
 				perror("Sending file failed!\n");
 				break;
 			}
-			memset(buffer, 0, sizeof(buffer));
 		}
+		memset(buffer, 0, sizeof(buffer));
 		fclose(fp);
 		printf("File transmitted!\n\n");
 	}
